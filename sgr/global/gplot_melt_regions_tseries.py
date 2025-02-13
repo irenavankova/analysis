5cd6e8f71de7959ee9d0ee367ca5d4edf78a0fd4
Irena Vankova <irena.vanek@gmail.com>
2025-02-13 13:54:07 -0700

    curve fits

--- sgr/global/gplot_melt_regions_tseries.py
+++ sgr/global/gplot_melt_regions_tseries.py
@@ -20,10 +20,17 @@ shelf_name = "Amery"
 #sims = ["sgrx8_rev_gmmoff", "S12_control", "S12_mali", "S12_mali_x4" , "S12_mali_x8"]
 #legsims = ["sgd x8, gm off past 50y", "control", "sgd x1", "sgd x4" , "sgd x8"]
 
-tseries = ["0001-0110", "0001-0110", "0001-0050", "0001-0050"]
-tsegment = ["clim_101-110_ts_1-110", "clim_101-110_ts_1-110", "clim_41-50_ts_1-50", "clim_41-50_ts_1-50"]
-sims = ["S12_control", "S12_mali", "S12_mali_x4" , "S12_mali_x8"]
-legsims = ["control", "subglacial x1", "subglacial x4" , "subglacial x8"]
+#tseries = ["0001-0110", "0001-0110", "0001-0050", "0001-0050"]
+#tsegment = ["clim_101-110_ts_1-110", "clim_101-110_ts_1-110", "clim_41-50_ts_1-50", "clim_41-50_ts_1-50"]
+#sims = ["S12_control", "S12_mali", "S12_mali_x4" , "S12_mali_x8"]
+#legsims = ["control", "subglacial x1", "subglacial x4" , "subglacial x8"]
+
+tseries = ["0001-0060", "0001-0110", "0001-0050"]
+tsegment = ["clim_51-60_ts_1-60", "clim_101-110_ts_1-110", "clim_41-50_ts_1-50"]
+sims = ["sgrx8_rev_gmmoff", "S12_control" , "S12_mali_x8"]
+legsims = ["reduced transport", "control", "subglacial"]
+
+
 fHeight = 4
 fWidth = 8
 plt.figure(figsize=(fWidth, fHeight))
@@ -45,11 +52,11 @@ for s in range(len(sims)):
 
     #MeltFlux[:, s] = np.squeeze(MeltFluxNow[:,ind_x])
     if s == 0:
-        #smb = ':'
-        smb = '-'
+        smb = 'k:'
+        #smb = '-'
     else:
         smb = '-'
-    plt.plot(Time, np.squeeze(MeltFluxNow[:, ind_x]),smb, linewidth=1, label = legsims[s])
+    plt.plot(Time, np.squeeze(MeltFluxNow[:, ind_x]),smb, linewidth=2, label = legsims[s])
     ind_t = np.where((Time >= 25) & (Time <= 50))
     mmean = np.mean(MeltFluxNow[ind_t, ind_x])
     print(mmean)
@@ -60,17 +67,17 @@ for s in range(len(sims)):
 
 #plt.xlim([19, Time[-1]])
 #plt.xlim([0, 100])
-plt.xlim([20, 50])
+plt.xlim([10, 60])
 #plt.title(shelf_name)
 plt.xlabel('Time (a)')
 plt.ylabel('Integrated melf flux (GT/a)')
-plt.legend(loc = 2,fontsize=14)
-plt.grid()
+plt.legend(loc = 2,fontsize=12)
+#plt.grid()
 
 opt_save = 1
 if opt_save == 1:
     #plt.savefig(f'/Users/irenavankova/Work/data_sim/SGR/global/Tseries/iceshelf/{shelf_name}rev.png', bbox_inches='tight',
-    plt.savefig(f'/Users/irenavankova/Work/data_sim/SGR/global/Tseries/iceshelf/{shelf_name}revTalk.png', bbox_inches='tight',
+    plt.savefig(f'/Users/irenavankova/Work/data_sim/SGR/global/Tseries/iceshelf/{shelf_name}revTalk2.png', bbox_inches='tight',
                 dpi=600)
 else:
     plt.show()
--- sgr/global/gplot_melt_sgr_scaling.py
+++ sgr/global/gplot_melt_sgr_scaling.py
@@ -7,13 +7,23 @@ import matplotlib.pyplot as plt
 import gsw
 import os
 import gmask_is
+from scipy.optimize import curve_fit
+
 
 # Get subglacial discharge per ice shelf
 rho_fw = 1000
 d2y = 365
+dir_fig_save = '/Users/irenavankova/Work/data_sim/SGR/global/Scaling'
 
 #iceshelves = ["Amery", "Thwaites", "Getz", "Totten"]
-iceshelves = ["George_VI", "Pine_Island", "Thwaites", "Getz", "Ross", "Totten", "Amery","Fimbul","Filchner-Ronne", "Larsen_C"]
+iceshelves = ["George_VI", "Pine_Island", "Thwaites", "Totten", "Fimbul", "Ross", "Amery", "Filchner-Ronne", "Larsen_C", "Getz"]
+#iceshelves = ["George_VI", "Pine_Island", "Thwaites", "Totten","Fimbul"]
+#iceshelves = ["George_VI", "Pine_Island", "Thwaites", "Totten","Fimbul"]
+#iceshelves = ["George_VI", "Pine_Island", "Thwaites", "Totten","Fimbul", "Ross"]
+
+nmnow = 'All'
+opt_save = 1
+opt_fits = 0
 
 sgr_file = f'/Users/irenavankova/Work/data_sim/E3SM_files/sgr_files_IV/MALI_20240327/DSGR.massFlux.MALI.out2055.SOwISC12to60E2r4.20240328.nc'
 ds = xarray.open_dataset(sgr_file)
@@ -59,21 +69,42 @@ for n in range(len(iceshelves)):
         ind_t = np.where((Time >= 21) & (Time <= 26))
         melt[n,s] = np.mean(MeltFluxNow[ind_t, ind_x])
 
-fHeight = 5
-fWidth = 10
-plt.figure(figsize=(fWidth, fHeight))
+def f_n_pow_1_3(x, a):
+    return a * np.power(x, 1/3)
 
+def f_n_pow_2_3(x, a):
+    return a * np.power(x, 2/3)
+
+xfit = np.linspace(0,1200,100)
+
+fHeight = 5
+fWidth = 8
+plt.figure(figsize=(fWidth, fHeight))
+clr = 'rkgbmcyrkgb'
+smb = 'ooooo^^^sss'
 for n in range(len(iceshelves)):
     #sgr_xax = np.array([0, 1, 4, 8]) * sgr_vol_flux[n]
     #plt.plot(sgr_xax, melt[n,:], linewidth=1, label = iceshelves[n])
     sgr_xax = np.array([0, 1, 4, 8]) * sgr_vol_flux[n]
-    plt.plot(sgr_xax, melt[n,:]-melt[n,0], 'x', linewidth=1, linestyle = '-', label = iceshelves[n])
+
+    if opt_fits == 1:
+        plt.plot(sgr_xax, melt[n, :] - melt[n, 0], f'{clr[n]}o', linewidth=3, label=iceshelves[n])
+        popt, pcov = curve_fit(f_n_pow_1_3, sgr_xax, melt[n,:]-melt[n,0], p0=10 ** 10)
+        plt.plot(xfit, f_n_pow_1_3(xfit, *popt), f'{clr[n]}--', linewidth=1)
+        popt, pcov = curve_fit(f_n_pow_2_3, sgr_xax, melt[n,:]-melt[n,0])
+        plt.plot(xfit, f_n_pow_2_3(xfit, *popt), f'{clr[n]}-', linewidth=1)
+    else:
+        plt.plot(sgr_xax, melt[n, :] - melt[n, 0], f'{clr[n]}{smb[n]}', linewidth=1, linestyle='-', label=iceshelves[n])
+
 
 plt.xlabel('SGR (m^3/s)')
 plt.ylabel('Integrated melf flux (GT/a)')
-plt.legend(loc = 2,fontsize=8)
+plt.legend(loc = 1,fontsize=8)
 plt.grid()
-plt.show()
+if opt_save == 1:
+    plt.savefig(f'{dir_fig_save}/{nmnow}.png', bbox_inches='tight', dpi=300)
+else:
+    plt.show()
 
 fHeight = 5
 fWidth = 10
@@ -89,4 +120,5 @@ plt.xlabel('SGR (m^3/s)')
 plt.ylabel('Integrated melf flux (GT/a)')
 plt.legend(loc = 2,fontsize=8)
 plt.grid()
-plt.show()
+if opt_save != 1:
+    plt.show()
