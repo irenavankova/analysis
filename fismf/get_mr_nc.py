16d5cfdfe384b434266d290c35972a1f7276cc2a
Irena Vankova <irena.vanek@gmail.com>
2025-06-19 11:45:49 -0600

    mr tesries on chrys

---
+++ fismf/get_mr_nc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import xarray as xr
+
+fpath = '/lcrc/group/acme/ac.dcomeau/scratch/chrys/E3SMv2_1/v2_1.SORRM.ssp370_ensmean/run/'
+
+secPerYear = 365 * 24 * 60 * 60
+kgingt = 1e12
+
+# Step 1: Load the mask from a different NetCDF file
+mask_file = f'{fpath}v2_1.SORRM.ssp370_ensmean.mpaso.rst.2015-01-01_00000.nc'  # Specify the mask file
+mask_ds = xr.open_dataset(mask_file)
+landIceFloatingMask = mask_ds['landIceFloatingMask']  # Assuming the variable name is 'mask' and it's of shape (ncells,)
+areaCell = mask_ds['areaCell']  # Assuming the variable name is 'mask' and it's of shape (ncells,)
+
+# Step 2: Open the 100 NetCDF files and concatenate them along the 'time' dimension
+ds = xr.open_mfdataset(f"{fpath}v2_1.SORRM.ssp370_ensmean.mpaso.hist.am.timeSeriesStatsMonthly.*.nc", combine='by_coords')
+#ds = xr.open_mfdataset("data_*.nc", combine='by_coords', chunks={'time': 10})
+
+# Step 3: Apply the mask by multiplying the temperature variable with the mask
+lifw = ds['timeMonthly_avg_landIceFreshwaterFlux'] * landIceFloatingMask * areaCell
+
+lifw = lifw * secPerYear / kgingt
+
+# Step 4: Sum the masked temperature over the 'ncells' dimension for each time step
+lifw_tseries = lifw.sum(dim='ncells')
+
+# Step 5: Save the resulting time series to a new NetCDF file
+lifw_tseries.to_netcdf('lifw_tseries.nc')
+
---
+++ fismf/gmask_tseries.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+import numpy as np
+import xarray
+import numpy # for arrays!
+#import scipy.io  # load matfiles
+import matplotlib.pyplot as plt
+import gsw
+import os
+
+opt_save = 0
+rho_fw = 1000.
+secPerYear = 365 * 24 * 60 * 60
+
+# MPAS Ocean data
+p_file = f'/Users/irenavankova/Work/data_sim/E3SM_files/E3SM_initial_condition/SOwISC12to60E2r4/ocean.SOwISC12to60E2r4.230220.nc'
+dsMesh = xarray.open_dataset(p_file)
+dsMesh = dsMesh[['latCell', 'lonCell','landIceFloatingMask','nVertLevels','areaCell','maxLevelCell','restingThickness']]
+dsMesh.load()
+
+lat = np.squeeze(dsMesh.latCell.data)
+lon = np.squeeze(dsMesh.lonCell.data)
+lat = lat*180/np.pi
+lon = lon*180/np.pi
+FloatingMask = np.squeeze(dsMesh.landIceFloatingMask.data)
+iii = (FloatingMask == 1)
+
+H = np.squeeze(dsMesh.restingThickness.data)
+H = np.nansum(H, axis=1)
+H = np.squeeze(H)
+
+fHeight = 5
+fWidth = fHeight
+plt.figure(figsize=(fWidth, fHeight))
+plt.plot(lon,lat,'b.')
+plt.plot(lon[iii],lat[iii],'r.')
+
+#Peninnsula West -Bellingshausen
+iam1 = (FloatingMask == 1) & (lat > -77.4) & (lon > 261) & (lon < 293.8)
+iam2 = (FloatingMask == 1) & (lat > -77.3) & (lon > 255) & (lon < 262.8)
+iam = np.logical_or(iam1, iam2)
+plt.plot(lon[iam],lat[iam],'m.')
+
+#Amundsen (up to Getz)
+iam = (FloatingMask == 1) & (lat > -76) & (lat < -73.2) & (lon > 225) & (lon < 261)
+plt.plot(lon[iam],lat[iam],'c.')
+
+#Ross
+iam1 = (FloatingMask == 1) & (lat > -85.6) & (lat < -77.4) & (lon > 158.64) & (lon < 212.5)
+iam2 = (lat < -77.8) | (lon < 200)
+iam = np.logical_and(iam1, iam2)
+plt.plot(lon[iam],lat[iam],'m.')
+
+#East Antarctica up to Amery
+iam = (FloatingMask == 1) & (lat > -72) & (lon > 80) & (lon < 166)
+plt.plot(lon[iam],lat[iam],'c.')
+
+#Amery
+iam = (FloatingMask == 1) & (lat > -74.3608) & (lat < -67.7122) & (lon > 62.0419) & (lon < 78)
+plt.plot(lon[iam],lat[iam],'g.')
+
+#DML
+iam = (FloatingMask == 1) & ((lon < 62.) | (lon > 332))
+plt.plot(lon[iam],lat[iam],'y.')
+
+#FRIS
+iam = (FloatingMask == 1) & (lat > -84) & (lat < -74.4) & (lon > 275) & (lon < 330)
+plt.plot(lon[iam],lat[iam],'k.')
+
+#Peninnsula East -Larsens
+iam = (FloatingMask == 1) &  (lat > -74.3) & (lon > 294) & (lon < 301)
+plt.plot(lon[iam],lat[iam],'c.')
+
+'''
+#Shelves
+
+# Amundsen sea shelf
+iam = (FloatingMask == 0) & (lat > -76) & (lat < -71) & (lon > 225) & (lon < 260)  & (H < 1500)
+plt.plot(lon[iam],lat[iam],'.',color='gray')
+
+#Ross shelf
+iam = (FloatingMask == 0) & (lat > -85.6) & (lat < -73) & (lon > 158.64) & (lon < 210) & (H < 1500)
+plt.plot(lon[iam],lat[iam],'k.')
+
+#AMERY Shelf
+iam = (FloatingMask == 0) & (lat > -70) & (lat < -65) & (lon > 67.5) & (lon < 80) & (H < 1500)
+plt.plot(lon[iam],lat[iam],'y.')
+
+#FRIS Shelf
+iam = (FloatingMask == 0) & (lat > -80) & (lat < -72) & (lon > 298) & (lon < 332) & (H < 1500)
+plt.plot(lon[iam],lat[iam],'g.')
+
+'''
+
+
+
+plt.show()
+
+
