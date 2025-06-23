0c04db61f69a766b319c9bf312a70008c20a5cef
Irena Vankova <irena.vanek@gmail.com>
2025-06-23 16:06:37 -0500

    get meltrate proxy

--- fismf/get_mr_nc_f.py
+++ fismf/get_mr_nc_f.py
@@ -3,20 +3,22 @@
 import xarray as xr
 import numpy as np
 
-fpath = '/lcrc/group/acme/ac.dcomeau/scratch/chrys/E3SMv2_1/v2_1.SORRM.ssp370_ensmean/run/'
+fpath_pismf = '/lcrc/group/acme/ac.dcomeau/scratch/chrys/E3SMv2_1/v2_1.SORRM.ssp370_ensmean/run/'
+fpath_fismf = '/lcrc/group/acme/ac.dcomeau/scratch/chrys/E3SMv2_1/v2_1.SORRM.ssp370_0701-fismf/archive/ocn/hist/'
 
 secPerYear = 365 * 24 * 60 * 60
 kgingt = 1e12
 
 # Step 1: Load the mask from a different NetCDF file
-mask_file = f'{fpath}v2_1.SORRM.ssp370_ensmean.mpaso.rst.2015-01-01_00000.nc'  # Specify the mask file
+mask_file = f'{fpath_pismf}v2_1.SORRM.ssp370_ensmean.mpaso.rst.2015-01-01_00000.nc'  # Specify the mask file
 mask_ds = xr.open_dataset(mask_file)
 landIceFloatingMask = mask_ds['landIceFloatingMask']  # Assuming the variable name is 'mask' and it's of shape (ncells,)
 areaCell = mask_ds['areaCell']  # Assuming the variable name is 'mask' and it's of shape (ncells,)
 
 # Step 2: Open the 100 NetCDF files and concatenate them along the 'time' dimension
-ds = xr.open_mfdataset(f"{fpath}v2_1.SORRM.ssp370_ensmean.mpaso.hist.am.timeSeriesStatsMonthly.*.nc", combine='by_coords', chunks={'time': 12}, parallel=True, decode_timedelta=True)
+dsPISMF = xr.open_mfdataset(f"{fpath_pismf}v2_1.SORRM.ssp370_ensmean.mpaso.hist.am.timeSeriesStatsMonthly.*.nc", combine='by_coords', chunks={'time': 12}, parallel=True, decode_timedelta=True)
 #ds = xr.open_mfdataset("data_*.nc", combine='by_coords', chunks={'time': 10})
+ds = xr.open_mfdataset(f"{fpath_fismf}v2_1.SORRM.ssp370_0701-fismf.mpaso.hist.am.timeSeriesStatsMonthly.*.nc", combine='by_coords', chunks={'time': 12}, parallel=True, decode_timedelta=True)
 
 print(ds.dims)
 
@@ -32,21 +34,9 @@ print(landIceFloatingMask.dims)
 #print(landIceFloatingMask.dims)
 
 # Melt rate
-lifw = ds['timeMonthly_avg_landIceFreshwaterFlux'] 
-lifw = lifw * landIceFloatingMask * areaCell
-#lifw = lifw * areaCell
-
-lifw = lifw * secPerYear / kgingt
-print(lifw.dims)
 
 # U* T*
-Ti = ds['timeMonthly_avg_landIceInterfaceTracers_landIceInterfaceTemperature']
-Tbl = ds['timeMonthly_avg_landIceBoundaryLayerTracers_landIceBoundaryLayerTemperature']
-ustar = ds['timeMonthly_avg_landIceFrictionVelocity']
-
-utstar = ustar * (Tbl - Ti)
-utstar = utstar * landIceFloatingMask * areaCell
-utstar = utstar * secPerYear / kgingt
+Ti = dsPISMF['timeMonthly_avg_landIceInterfaceTracers_landIceInterfaceTemperature']
 
 # umag T
 
@@ -69,8 +59,8 @@ T = ds['timeMonthly_avg_activeTracers_temperature']
 T = T.isel(nVertLevels=0)
 
 # Access the global attribute
-Cd = ds.attrs['config_land_ice_flux_topDragCoeff']
-utidal = ds.attrs['config_land_ice_flux_rms_tidal_velocity']
+Cd = dsPISMF.attrs['config_land_ice_flux_topDragCoeff']
+utidal = dsPISMF.attrs['config_land_ice_flux_rms_tidal_velocity']
 
 utsq = np.sqrt(Cd * (Usq + Vsq + utidal**2)) * (T - Ti)
 #ut = np.sqrt(Cd * (U**2 + V**2 + utidal**2)) * (T - Ti)
@@ -82,25 +72,17 @@ utsq = utsq * secPerYear / kgingt
 #ut = ut * secPerYear / kgingt
 
 # Step 4: Sum the masked temperature over the 'ncells' dimension for each time step
-lifw_tseries = lifw.sum(dim='nCells').compute()
-utstar_tseries = utstar.sum(dim='nCells').compute()
 utsq_tseries = utsq.sum(dim='nCells').compute()
 
-print(lifw_tseries.dims)
-lifw_tseries.name = 'lifw'
-print(utstar_tseries.dims)
-utstar_tseries.name = 'utstar'
 utsq_tseries.name = 'utsq'
 # Step 5: Save the resulting time series to a new NetCDF file
 #lifw_tseries.to_netcdf('lifw_tseries.nc')
 
 # Combine into a single Dataset
 tseries_ds = xr.Dataset({
-    'lifw': lifw_tseries,
-    'utstar': utstar_tseries,
     'utsq': utsq_tseries
 })
 
 # Save to NetCDF
-tseries_ds.to_netcdf('proxy_tseries.nc')
+tseries_ds.to_netcdf('proxy_tseries_fismf.nc')
 
