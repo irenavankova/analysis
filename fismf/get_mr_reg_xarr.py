224c79fdb4fc263e7ce4829be819baee31c67de8
Irena Vankova <irena.vanek@gmail.com>
2025-06-24 12:11:19 -0600

    reg for 701

--- fismf/get_mr_reg_xarr.py
+++ fismf/get_mr_reg_xarr.py
@@ -5,13 +5,19 @@ import numpy as np
 
 import gmask_reg
 
-fpath = '/lcrc/group/acme/ac.dcomeau/scratch/chrys/E3SMv2_1/v2_1.SORRM.ssp370_ensmean/run/'
+opt = 701
+
+fpath_mask = '/lcrc/group/acme/ac.dcomeau/scratch/chrys/E3SMv2_1/v2_1.SORRM.ssp370_ensmean/run/'
+if opt == 701:
+    fpath = '/lcrc/group/acme/ac.dcomeau/scratch/chrys/E3SMv2_1/v2_1.SORRM.ssp370_0701/archive/ocn/hist/'
+else:
+    fpath = fpath_mask
 
 secPerYear = 365 * 24 * 60 * 60
 kgingt = 1e12
 
 # Step 1: Load the mask from a different NetCDF file
-mask_file = f'{fpath}v2_1.SORRM.ssp370_ensmean.mpaso.rst.2015-01-01_00000.nc'  # Specify the mask file
+mask_file = f'{fpath_mask}v2_1.SORRM.ssp370_ensmean.mpaso.rst.2015-01-01_00000.nc'  # Specify the mask file
 mask_ds = xr.open_dataset(mask_file)
 landIceFloatingMask = mask_ds['landIceFloatingMask']  # Assuming the variable name is 'mask' and it's of shape (ncells,)
 areaCell = mask_ds['areaCell']  # Assuming the variable name is 'mask' and it's of shape (ncells,)
@@ -20,7 +26,10 @@ iceshelves = ["Antarctica", "Belli", "Amundsen", "Ross", "Eastant", "Amery", "Dm
 iam = gmask_reg.get_mask(iceshelves, mask_file)
 
 # Step 2: Open the 100 NetCDF files and concatenate them along the 'time' dimension
-ds = xr.open_mfdataset(f"{fpath}v2_1.SORRM.ssp370_ensmean.mpaso.hist.am.timeSeriesStatsMonthly.*.nc", combine='by_coords', chunks={'time': 12}, parallel=True, decode_timedelta=True)
+if opt == 701:
+    ds = xr.open_mfdataset(f"{fpath}v2_1.SORRM.ssp370_0701.mpaso.hist.am.timeSeriesStatsMonthly.*.nc", combine='by_coords', chunks={'time': 12}, parallel=True, decode_timedelta=True)
+else:
+    ds = xr.open_mfdataset(f"{fpath}v2_1.SORRM.ssp370_ensmean.mpaso.hist.am.timeSeriesStatsMonthly.*.nc", combine='by_coords', chunks={'time': 12}, parallel=True, decode_timedelta=True)
 
 landIceFloatingMask = landIceFloatingMask.squeeze('Time')
 
@@ -46,5 +55,8 @@ lifw_tseries = lifw_tseries.transpose('Time', 'region')
 lifw_tseries.name = 'lifw'
 
 # Save to NetCDF
-lifw_tseries.to_dataset().to_netcdf('lifw_reg_xarr_tseries.nc')
+if opt == 701:
+    lifw_tseries.to_dataset().to_netcdf('lifw_reg_701_tseries.nc')
+else:
+    lifw_tseries.to_dataset().to_netcdf('lifw_reg_xarr_tseries.nc')
 
