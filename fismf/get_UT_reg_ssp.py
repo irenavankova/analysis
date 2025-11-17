71ca7768a5b16b2e931b38cb64756186b9839978
Irena Vankova <irena.vanek@gmail.com>
2025-11-17 20:36:26 +0100

    ens members fot utsq pismf

--- fismf/get_UT_reg_ssp.py
+++ fismf/get_UT_reg_ssp.py
@@ -5,13 +5,21 @@ import numpy as np
 
 import gmask_reg
 
-fpath = '/lcrc/group/acme/ac.dcomeau/scratch/chrys/E3SMv2_1/v2_1.SORRM.ssp370_ensmean/run/'
+ymem = '701'
+
+fpath_pismf = '/lcrc/group/acme/ac.dcomeau/scratch/chrys/E3SMv2_1/v2_1.SORRM.ssp370_ensmean/run/'
+
+if ymem == 'ave':
+    fpath = f'{fpath_pismf}'
+else:
+    fpath = f'/lcrc/group/acme/ac.dcomeau/scratch/chrys/E3SMv2_1/v2_1.SORRM.ssp370_0{ymem}/'
+    fpath = f'{fpath}archive/ocn/hist/'
 
 secPerYear = 365 * 24 * 60 * 60
 kgingt = 1e12
 
 # Step 1: Load the mask from a different NetCDF file
-mask_file = f'{fpath}v2_1.SORRM.ssp370_ensmean.mpaso.rst.2015-01-01_00000.nc'  # Specify the mask file
+mask_file = f'{fpath_pismf}v2_1.SORRM.ssp370_ensmean.mpaso.rst.2015-01-01_00000.nc'  # Specify the mask file
 mask_ds = xr.open_dataset(mask_file)
 landIceFloatingMask = mask_ds['landIceFloatingMask']  # Assuming the variable name is 'mask' and it's of shape (ncells,)
 areaCell = mask_ds['areaCell']  # Assuming the variable name is 'mask' and it's of shape (ncells,)
@@ -29,7 +37,14 @@ def preprocess(dss):
             "timeMonthly_avg_activeTracers_temperature"
         ]
     ]
-ds = xr.open_mfdataset(f"{fpath}v2_1.SORRM.ssp370_ensmean.mpaso.hist.am.timeSeriesStatsMonthly.21*.nc", combine='by_coords', chunks={'time': 12}, parallel=True, decode_timedelta=True, preprocess=preprocess)
+
+if ymem == 'ave':
+    ds = xr.open_mfdataset(f"{fpath}v2_1.SORRM.ssp370_ensmean.mpaso.hist.am.timeSeriesStatsMonthly.*.nc",
+                           combine='by_coords', chunks={'time': 12}, parallel=True, decode_timedelta=True,
+                           preprocess=preprocess)
+else:
+    ds = xr.open_mfdataset(f"{fpath}v2_1.SORRM.ssp370_0{ymem}.mpaso.hist.am.timeSeriesStatsMonthly.*.nc", combine='by_coords', chunks={'time': 12}, parallel=True, decode_timedelta=True, preprocess=preprocess)
+
 
 landIceFloatingMask = landIceFloatingMask.squeeze('Time')
 
@@ -68,5 +83,5 @@ utsq_tseries = utsq_tseries.transpose('Time', 'region')
 utsq_tseries.name = 'utsq'
 
 # Save to NetCDF
-utsq_tseries.to_dataset().to_netcdf('utsq_reg_pismf_ave_tseries.nc')
+utsq_tseries.to_dataset().to_netcdf(f'utsq_reg_pismf_{ymem}_tseries.nc')
 
